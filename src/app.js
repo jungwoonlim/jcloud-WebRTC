4e1317257a7c38f62a8c1b5c6b518cd18976091c
jungwoonlim <wypo23@morpheus3d.co.kr>
2020-11-05 12:55:52 +0900

    jCloud WebRTC init

---
+++ src/app.js
@@ -0,0 +1,24 @@
+"use strict";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+const app = express();
+
+app.use(helmet());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use("/", (req, res) => {
+  res.render("Home");
+});
+
+export default app;
